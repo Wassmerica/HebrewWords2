dddd767a766001b09e12a04505b55c36d19b8e1f
Michael Wasserman <wmichael@ca.ibm.com>
2018-05-11 09:33:30 -0400

    initial commit

---
+++ HebrewWords/HebrewWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HebrewWords
+//
+//  Created by Michael Wasserman on 2018-05-11.
+//  Copyright © 2018 Wasserman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
