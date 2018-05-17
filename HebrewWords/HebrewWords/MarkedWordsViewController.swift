559864081e277e92a7e420ade29b8da9135faf11
Michael Wasserman <wmichael@ca.ibm.com>
2018-05-17 15:57:56 -0400

    - removed commented out code

--- HebrewWords/HebrewWords/MarkedWordsViewController.swift
+++ HebrewWords/HebrewWords/MarkedWordsViewController.swift
@@ -60,15 +60,4 @@ class MarkedWordsViewController: UIViewController, UITableViewDelegate, UITableV
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
